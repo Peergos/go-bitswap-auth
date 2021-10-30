c163582579922ee927ec6c05addf0fd1d1f93995
ian <ianopolous@protonmail.com>
2021-10-30 02:54:24 +0100

    update session manager tests

--- internal/sessionmanager/sessionmanager_test.go
+++ internal/sessionmanager/sessionmanager_test.go
@@ -15,32 +15,32 @@ import (
 	bssession "github.com/peergos/go-bitswap-auth/internal/session"
 	bssim "github.com/peergos/go-bitswap-auth/internal/sessioninterestmanager"
 	"github.com/peergos/go-bitswap-auth/internal/testutil"
+        "github.com/peergos/go-bitswap-auth/auth"
 
 	blocks "github.com/ipfs/go-block-format"
-	cid "github.com/ipfs/go-cid"
 	peer "github.com/libp2p/go-libp2p-core/peer"
 )
 
 type fakeSession struct {
-	ks         []cid.Cid
-	wantBlocks []cid.Cid
-	wantHaves  []cid.Cid
+	ks         []auth.Want
+	wantBlocks []auth.Want
+	wantHaves  []auth.Want
 	id         uint64
 	pm         *fakeSesPeerManager
 	sm         bssession.SessionManager
 	notif      notifications.PubSub
 }
 
-func (*fakeSession) GetBlock(context.Context, cid.Cid) (blocks.Block, error) {
+func (*fakeSession) GetBlock(context.Context, auth.Want) (auth.AuthBlock, error) {
 	return nil, nil
 }
-func (*fakeSession) GetBlocks(context.Context, []cid.Cid) (<-chan blocks.Block, error) {
+func (*fakeSession) GetBlocks(context.Context, []auth.Want) (<-chan auth.AuthBlock, error) {
 	return nil, nil
 }
 func (fs *fakeSession) ID() uint64 {
 	return fs.id
 }
-func (fs *fakeSession) ReceiveFrom(p peer.ID, ks []cid.Cid, wantBlocks []cid.Cid, wantHaves []cid.Cid) {
+func (fs *fakeSession) ReceiveFrom(p peer.ID, ks []auth.Want, wantBlocks []auth.Want, wantHaves []auth.Want) {
 	fs.ks = append(fs.ks, ks...)
 	fs.wantBlocks = append(fs.wantBlocks, wantBlocks...)
 	fs.wantHaves = append(fs.wantHaves, wantHaves...)
@@ -62,19 +62,19 @@ func (*fakeSesPeerManager) ProtectConnection(peer.ID) {}
 
 type fakePeerManager struct {
 	lk      sync.Mutex
-	cancels []cid.Cid
+	cancels []auth.Want
 }
 
 func (*fakePeerManager) RegisterSession(peer.ID, bspm.Session)                    {}
 func (*fakePeerManager) UnregisterSession(uint64)                                 {}
-func (*fakePeerManager) SendWants(context.Context, peer.ID, []cid.Cid, []cid.Cid) {}
-func (*fakePeerManager) BroadcastWantHaves(context.Context, []cid.Cid)            {}
-func (fpm *fakePeerManager) SendCancels(ctx context.Context, cancels []cid.Cid) {
+func (*fakePeerManager) SendWants(context.Context, peer.ID, []auth.Want, []auth.Want) {}
+func (*fakePeerManager) BroadcastWantHaves(context.Context, []auth.Want)            {}
+func (fpm *fakePeerManager) SendCancels(ctx context.Context, cancels []auth.Want) {
 	fpm.lk.Lock()
 	defer fpm.lk.Unlock()
 	fpm.cancels = append(fpm.cancels, cancels...)
 }
-func (fpm *fakePeerManager) cancelled() []cid.Cid {
+func (fpm *fakePeerManager) cancelled() []auth.Want {
 	fpm.lk.Lock()
 	defer fpm.lk.Unlock()
 	return fpm.cancels
@@ -126,24 +126,25 @@ func TestReceiveFrom(t *testing.T) {
 	secondSession := sm.NewSession(ctx, time.Second, delay.Fixed(time.Minute)).(*fakeSession)
 	thirdSession := sm.NewSession(ctx, time.Second, delay.Fixed(time.Minute)).(*fakeSession)
 
-	sim.RecordSessionInterest(firstSession.ID(), []cid.Cid{block.Cid()})
-	sim.RecordSessionInterest(thirdSession.ID(), []cid.Cid{block.Cid()})
+        w := auth.NewWant(block.Cid(), "auth")
+	sim.RecordSessionInterest(firstSession.ID(), []auth.Want{w})
+	sim.RecordSessionInterest(thirdSession.ID(), []auth.Want{w})
 
-	sm.ReceiveFrom(ctx, p, []cid.Cid{block.Cid()}, []cid.Cid{}, []cid.Cid{})
+	sm.ReceiveFrom(ctx, p, []auth.Want{w}, []auth.Want{}, []auth.Want{})
 	if len(firstSession.ks) == 0 ||
 		len(secondSession.ks) > 0 ||
 		len(thirdSession.ks) == 0 {
 		t.Fatal("should have received blocks but didn't")
 	}
 
-	sm.ReceiveFrom(ctx, p, []cid.Cid{}, []cid.Cid{block.Cid()}, []cid.Cid{})
+	sm.ReceiveFrom(ctx, p, []auth.Want{}, []auth.Want{w}, []auth.Want{})
 	if len(firstSession.wantBlocks) == 0 ||
 		len(secondSession.wantBlocks) > 0 ||
 		len(thirdSession.wantBlocks) == 0 {
 		t.Fatal("should have received want-blocks but didn't")
 	}
 
-	sm.ReceiveFrom(ctx, p, []cid.Cid{}, []cid.Cid{}, []cid.Cid{block.Cid()})
+	sm.ReceiveFrom(ctx, p, []auth.Want{}, []auth.Want{}, []auth.Want{w})
 	if len(firstSession.wantHaves) == 0 ||
 		len(secondSession.wantHaves) > 0 ||
 		len(thirdSession.wantHaves) == 0 {
@@ -173,16 +174,17 @@ func TestReceiveBlocksWhenManagerShutdown(t *testing.T) {
 	secondSession := sm.NewSession(ctx, time.Second, delay.Fixed(time.Minute)).(*fakeSession)
 	thirdSession := sm.NewSession(ctx, time.Second, delay.Fixed(time.Minute)).(*fakeSession)
 
-	sim.RecordSessionInterest(firstSession.ID(), []cid.Cid{block.Cid()})
-	sim.RecordSessionInterest(secondSession.ID(), []cid.Cid{block.Cid()})
-	sim.RecordSessionInterest(thirdSession.ID(), []cid.Cid{block.Cid()})
+	w := auth.NewWant(block.Cid(), "auth")
+	sim.RecordSessionInterest(firstSession.ID(), []auth.Want{w})
+	sim.RecordSessionInterest(secondSession.ID(), []auth.Want{w})
+	sim.RecordSessionInterest(thirdSession.ID(), []auth.Want{w})
 
 	sm.Shutdown()
 
 	// wait for sessions to get removed
 	time.Sleep(10 * time.Millisecond)
 
-	sm.ReceiveFrom(ctx, p, []cid.Cid{block.Cid()}, []cid.Cid{}, []cid.Cid{})
+	sm.ReceiveFrom(ctx, p, []auth.Want{w}, []auth.Want{}, []auth.Want{})
 	if len(firstSession.ks) > 0 ||
 		len(secondSession.ks) > 0 ||
 		len(thirdSession.ks) > 0 {
@@ -208,16 +210,17 @@ func TestReceiveBlocksWhenSessionContextCancelled(t *testing.T) {
 	secondSession := sm.NewSession(sessionCtx, time.Second, delay.Fixed(time.Minute)).(*fakeSession)
 	thirdSession := sm.NewSession(ctx, time.Second, delay.Fixed(time.Minute)).(*fakeSession)
 
-	sim.RecordSessionInterest(firstSession.ID(), []cid.Cid{block.Cid()})
-	sim.RecordSessionInterest(secondSession.ID(), []cid.Cid{block.Cid()})
-	sim.RecordSessionInterest(thirdSession.ID(), []cid.Cid{block.Cid()})
+	w := auth.NewWant(block.Cid(), "auth")
+	sim.RecordSessionInterest(firstSession.ID(), []auth.Want{w})
+	sim.RecordSessionInterest(secondSession.ID(), []auth.Want{w})
+	sim.RecordSessionInterest(thirdSession.ID(), []auth.Want{w})
 
 	sessionCancel()
 
 	// wait for sessions to get removed
 	time.Sleep(10 * time.Millisecond)
 
-	sm.ReceiveFrom(ctx, p, []cid.Cid{block.Cid()}, []cid.Cid{}, []cid.Cid{})
+	sm.ReceiveFrom(ctx, p, []auth.Want{w}, []auth.Want{}, []auth.Want{})
 	if len(firstSession.ks) == 0 ||
 		len(secondSession.ks) > 0 ||
 		len(thirdSession.ks) == 0 {
@@ -238,10 +241,11 @@ func TestShutdown(t *testing.T) {
 
 	p := peer.ID(fmt.Sprint(123))
 	block := blocks.NewBlock([]byte("block"))
-	cids := []cid.Cid{block.Cid()}
+	w := auth.NewWant(block.Cid(), "auth")
+	cids := []auth.Want{w}
 	firstSession := sm.NewSession(ctx, time.Second, delay.Fixed(time.Minute)).(*fakeSession)
 	sim.RecordSessionInterest(firstSession.ID(), cids)
-	sm.ReceiveFrom(ctx, p, []cid.Cid{}, []cid.Cid{}, cids)
+	sm.ReceiveFrom(ctx, p, []auth.Want{}, []auth.Want{}, cids)
 
 	if !bpm.HasKey(block.Cid()) {
 		t.Fatal("expected cid to be added to block presence manager")
@@ -255,7 +259,7 @@ func TestShutdown(t *testing.T) {
 	if bpm.HasKey(block.Cid()) {
 		t.Fatal("expected cid to be removed from block presence manager")
 	}
-	if !testutil.MatchKeysIgnoreOrder(pm.cancelled(), cids) {
+	if !testutil.MatchWantsIgnoreOrder(pm.cancelled(), cids) {
 		t.Fatal("expected cancels to be sent")
 	}
 }
